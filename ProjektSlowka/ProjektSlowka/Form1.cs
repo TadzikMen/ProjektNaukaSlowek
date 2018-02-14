32e6bb60747c9a8dafedcceee87ebceb41efd21f
Tadeusz <Tadeusz@DESKTOP-RFO0OSS>
2018-02-14 21:06:52 +0100

    Test

--- ProjektSlowka/ProjektSlowka/Form1.cs
+++ ProjektSlowka/ProjektSlowka/Form1.cs
@@ -20,6 +20,7 @@ namespace ProjektSlowka
         void funkcja()
         {
             Console.WriteLine("cos");
+            Console.WriteLine("cos");
             Console.Write("");
         }
         private void Form1_Load(object sender, EventArgs e)
