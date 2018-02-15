650df243beaf37c79ca0e842bf5fe694daed3074
Daga <Daga@DESKTOP-B3UN5RT>
2018-02-15 20:33:29 +0100

    test 6

--- Projekt_Serwer/Projekt_Serwer/Form1.cs
+++ Projekt_Serwer/Projekt_Serwer/Form1.cs
@@ -16,5 +16,10 @@ namespace Projekt_Serwer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int h = 0;
+        }
     }
 }
