f4f46c4f67216d63bcec6fbe5ae5e9fbbdedac35
Tadeusz <Tadeusz@DESKTOP-RFO0OSS>
2018-03-25 20:41:08 +0200

    Fix właściwości(Obsługa Logowania)

--- ProjektNaukaSlowek/ClientApp/Models/ObslugaLogowania.cs
+++ ProjektNaukaSlowek/ClientApp/Models/ObslugaLogowania.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace ClientApp.Models
 {
-	public class ObslugaLogowania
-	{
-		private WcfService.Logowanie _login;
-		public WcfService.Logowanie Login { get => _login; set => _login = value; }
-	}
+    public class ObslugaLogowania
+    {
+        private WcfService.Logowanie _login;
+        public WcfService.Logowanie Login
+        {
+            get
+            {
+                return _login;
+            }
+
+            set
+            {
+                _login = value;
+            }
+        }
+    }
 }
