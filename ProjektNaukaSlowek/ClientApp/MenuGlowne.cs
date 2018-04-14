146d8b7ac651359826a8496b903d21c5318a19ce
Daga <Daga@DESKTOP-B3UN5RT>
2018-04-14 14:15:14 +0200

    Formatki

--- ProjektNaukaSlowek/ClientApp/MenuGlowne.cs
+++ ProjektNaukaSlowek/ClientApp/MenuGlowne.cs
@@ -36,16 +36,14 @@ namespace ClientApp
 		}
 
 		private void Logowanie_Click(object sender, EventArgs e)
-		{
-            //if (frmLogowanie == null)
-            //{
-            //	frmLogowanie = new Logowanie();
-            //	frmLogowanie.FormClosed += Logowanie_FormClosed;
-            //}
-            //frmLogowanie.Show(this);
-            Slownik f = new Slownik();
-            f.ShowDialog();
-			Hide();
+        {
+            if (frmLogowanie == null)
+            {
+                frmLogowanie = new Logowanie();
+                frmLogowanie.FormClosed += Logowanie_FormClosed;
+            }
+            frmLogowanie.Show(this);
+            Hide();
 		}
 
 		private void Logowanie_FormClosed(object sender, FormClosedEventArgs e)
